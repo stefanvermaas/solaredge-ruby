955d66fc92c056a8eea499c164607501ef3832b2
Stefan Vermaas <stefan@bobbiehq.com>
2021-05-23 13:39:08 +0200

    Extends and updates the potential errors with HTTP client errors.

--- lib/solaredge/error.rb
+++ lib/solaredge/error.rb
@@ -8,10 +8,18 @@ module Solaredge
   class BadConfiguration < Error
     def initialize
       super(
-        "The provided configuration object is not a Solaredge::Configuration " \
-          "instance. Please provide a Solaredge::Configuration instance when "  \
-          "overriding the configuration directly."
+        "[Solaredge] The provided configuration object is not a " \
+          "Solaredge::Configuration instance. Please provide a "  \
+          "Solaredge::Configuration instance when overriding the configuration directly."
       )
+    end
+  end
+
+  # The `Solaredge::HttpClientError` is raised whenever the Solaredge API
+  # returns a status code other than success.
+  class HttpClientError < Error
+    def initialize(code:, message:)
+      super("[Solaredge] #{message}  - #{code}")
     end
   end
 
@@ -20,8 +28,18 @@ module Solaredge
   class InvalidResponseFormat < Error
     def initialize(response_format)
       super(
-        "'#{response_format}' is not a valid response format. " \
+        "[Solaredge] '#{response_format}' is not a valid response format. " \
           "Please use one of the available response formats (e.g. json, xml or csv)."
+      )
+    end
+  end
+
+  # The `MissingApiKey` is raised whenever the API consumer forgot to add the API key.
+  class MissingApiKey < Error
+    def initialize
+      super(
+        "[Solaredge] Missing API key: Please, provide an API key to make requests " \
+          "to the Solaredge Monitoring API."
       )
     end
   end
